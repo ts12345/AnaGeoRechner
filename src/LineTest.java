2744a5bf29afea2d82151d3adb4653565ab0f802
ts <t@s>
2021-03-26 12:45:43 +0100

    Test Line:angleTo( ... )

--- src/LineTest.java
+++ src/LineTest.java
@@ -65,5 +65,26 @@ public class LineTest
         Line l2 = new Line(A2, B2);
         // l1.fusspunktFindenZu(l2);
     }
-}
 
+    @Test
+    public void test_angleTo()
+    {
+        Point A1 = new src.Point(1, 2, 0);
+        Vector B1 = new Vector(1, 1, 1);
+
+        Point A2 = new src.Point(1, 2, 0);
+        Vector B2 = new Vector(0, 0, 1);
+
+        Line l1 = new Line(A1, B1);
+        Line l2 = new Line(A2, B2);
+
+        Vector n = new Vector(1, 1, 1);
+        Vector A = new Vector(2, 0, 0);
+        Plane p = new Plane(n, A);
+
+        assertEquals(45, l1.angleTo(l2), 1e-10);        
+        assertEquals(0, l1.angleTo(p), 1e-10);
+        assertEquals(35.26438968275, l1.angleTo(p), 1e-10);
+    }
+
+}
